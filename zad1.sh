ab9655955964f7795e8a2c91e8528e33435091d2
Jakub <jakubgorski1212@gmail.com>
2024-04-21 17:37:32 +0200

    Ostatnia zmiana

--- zad1.sh
+++ zad1.sh
@@ -91,3 +91,87 @@ git switch one;
 git cherry-pick C4 C3 C2;
 git switch two;
 git cherry-pick C5 C4 C3 C2;
+
+
+### Zdalny Git
+
+#1.1
+git clone;
+
+#1.2
+git commit;
+git switch o/main;
+git commit;
+
+#1.3
+git fetch;
+
+#1.4
+git pull;
+
+#1.5
+git clone;
+git fakeTeamwork 2;
+git commit;
+git pull;
+
+#1.6
+git commit;
+git commit;
+git push;
+
+#1.7
+git clone;
+git fakeTeamwork;
+git commit;
+git pull --rebase;
+git push;
+
+#1.8
+git reset o/main;
+git checkout -b feature C2;
+git push;
+
+#2.1
+git fetch;
+git rebase o/main side1;
+git rebase side1 side2;
+git rebase side2 side3;
+git rebase side3 main;
+git push;
+
+#2.2
+git switch main;
+git pull;
+git merge side1;
+git merge side2;
+git merge side3;
+git push;
+
+#2.3
+git checkout -b side o/main;
+git commit;
+git pull --rebase;
+git push;
+
+#2.4
+git push origin main;
+git push origin foo;
+
+#2.5
+git push origin foo:main;
+git push origin main^:foo;
+
+#2.6
+git fetch origin foo:main;
+git pull origin main~:foo;
+git switch foo;
+git merge main;
+
+#2.7
+git push origin :foo;
+git pull origin :bar;
+
+#2.8
+git pull origin bar:foo;
+git pull origin main:side;
